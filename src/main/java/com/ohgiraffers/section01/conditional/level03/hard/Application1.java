b8150cc0752b19ba92de00103f8cd12c147f176c
DESKTOP-67GVLQD\seongmin <dltgld72@naver.com>
2023-05-04 12:58:43 +0900

    [conditional][hard][문제1] 완료
    Scanner와 nextInt(), nextLine()을 사용하여 정수와 문자열을 받고, 문자열의 첫 번째 문자를 char로 변환시켜 받고, char oper를 연산기호와 비교하여 연산을 하였습니다.

--- src/main/java/com/ohgiraffers/section01/conditional/level03/hard/Application1.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level03/hard/Application1.java
@@ -1,9 +1,11 @@
 package com.ohgiraffers.section01.conditional.level03.hard;
 
+import java.util.Scanner;
+
 public class Application1 {
-	
+
 	public static void main(String[] args) {
-		
+
 		/* 두 개의 정수를 입력 받아 변수에 저장하고,
 		 * 연산기호(+, -, *, /, %)를 입력 받아 해당 연산의 수행 결과를 출력하세요.
 		 * 단, 준비된 연산기호 외의 문자를 입력하는 경우 "입력하신 연산은 없습니다. 프로그램을 종료합니다." 출력 후 프로그램 종료
@@ -16,7 +18,21 @@ public class Application1 {
 		 * -- 출력 예시 --
 		 * 4 + 3 = 7 
 		 */
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("첫 번째 정수를 입력하세요 : ");
+		int num1 = scan.nextInt();
+		System.out.print("두 번째 정수를 입력하세요 : ");
+		int num2 = scan.nextInt();
+		scan.nextLine();
+		System.out.print("연산 기호를 입력해 주세요 : ");
+		char oper = scan.nextLine().charAt(0);
+		if( oper == '+') System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+		else if( oper == '-') System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+		else if( oper == '*') System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+		else if( oper == '/') System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+		else if( oper == '%') System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+		else System.out.println("입력하신 연산은 없습니다. 프로그램을 종료합니다.");
+
 	}
-	
+
 }
