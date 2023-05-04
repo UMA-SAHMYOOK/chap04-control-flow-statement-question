7e34b95ac59ef305d0469f98079d3d95a1fa39e7
DESKTOP-67GVLQD\seongmin <dltgld72@naver.com>
2023-05-04 13:07:37 +0900

    [looping-and-branching][basic][문제2] 완료
    scanner을 이용해 정수를 입력받고, for문을 이용하여 합을 구했습니다.

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level01/basic/Application2.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level01/basic/Application2.java
@@ -1,9 +1,11 @@
 package com.ohgiraffers.section02.looping_and_branching.level01.basic;
 
+import java.util.Scanner;
+
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+
 		/* 정수 한 개를 입력 받고, 1부터 입력 받은 정수까지의 합을 계산해서 출력하세요
 		 * 
 		 * -- 입력 예시 --
@@ -13,7 +15,14 @@ public class Application2 {
 		 * 1부터 5까지의 합 : 15
 		 * 
 		 * */
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("정수를 입력하세요 :");
+		int num = scan.nextInt();
+		int result = 0;
+		for(int i = 1; i<= num; i++) result += i;
+		System.out.println("1부터 " + num + "까지의 합 : " + result);
+
+
 	}
 
 }
