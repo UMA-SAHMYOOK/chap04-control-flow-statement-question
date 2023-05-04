3d0b24fd670f2eb3084abbe1a923a62b339a83e0
CHOI <seongyong335@gmail.com>
2023-05-04 13:42:35 +0900

    [looping_and_branching][basic][문제2] 완료
    스캐너를 sc로 정의
    
    정수를 받을 int tmp를 정의
    int sum을 0으로 정의
    
    i가 1부터 시작해서 tmp까지 반복하는 반복문을 만들어
    i값을 sum에 더할때마다 i가 1씩 오르게하였습니다.

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level01/basic/Application2.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level01/basic/Application2.java
@@ -1,9 +1,11 @@
 package com.ohgiraffers.section02.looping_and_branching.level01.basic;
 
+import java.util.Scanner;
+
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+
 		/* 정수 한 개를 입력 받고, 1부터 입력 받은 정수까지의 합을 계산해서 출력하세요
 		 * 
 		 * -- 입력 예시 --
@@ -13,7 +15,19 @@ public class Application2 {
 		 * 1부터 5까지의 합 : 15
 		 * 
 		 * */
-		
+
+		Scanner sc = new Scanner(System.in);
+		int tmp;
+		int sum = 0;
+
+		System.out.print("정수를 입력하세요 : ");
+		tmp = sc.nextInt();
+
+		for(int i = 1; i <= tmp; i++){
+			sum += i;
+		}
+
+		System.out.println("1부터 " + tmp + "까지의 합 : " + sum);
 	}
 
 }
