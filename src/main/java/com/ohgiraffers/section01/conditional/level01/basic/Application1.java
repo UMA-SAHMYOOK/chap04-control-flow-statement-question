9a1b1302299c3bd591daed4ce1e82226bb6f3d23
AzureAD\양한진 <gkswls3203@naver.com>
2023-05-04 11:23:38 +0900

    [section1][basic][level 01] 완료 값을 입력받기 위해 scanner 사용, 입력받은 값을 저장하기 위해 next사용, 입력 값이 양수 일경우와 양수가 아닐경우를 위해 if else 사용

--- src/main/java/com/ohgiraffers/section01/conditional/level01/basic/Application1.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level01/basic/Application1.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section01.conditional.level01.basic;
 
+import java.util.Scanner;
+
 public class Application1 {
 
 	public static void main(String[] args) {
@@ -13,6 +15,19 @@ public class Application1 {
 		 * -- 출력 예시 --
 		 * 양수다.
 		 * */
+		java.util.Scanner sc = new Scanner(System.in);
+		System.out.print("정수를 하나 입력하세요 : ");
+		int num = sc.nextInt();
+		System.out.println(num);
+		if (num > 0 ){
+			System.out.println("양수다.");
+		}
+		else {
+			System.out.println("양수가 아니다.");
+
+		}
+
+
 
 	}
 
