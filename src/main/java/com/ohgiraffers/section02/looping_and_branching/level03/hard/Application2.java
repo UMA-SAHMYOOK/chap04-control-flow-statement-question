dd20a1f1ef879c8d9c8b7166792f5f3f24d035d9
CHOI <seongyong335@gmail.com>
2023-05-04 14:29:51 +0900

    [looping_and_branching][hard][문제2] 완료
    Scanner를 sc로 정의
    정수를 받을 int tmp 정의
    횟수를 저장할 int n = 0 정의
    랜덤 난수를 받을 int ran을 정의하고 난수 1~100까지 정수를 담는다.
    
    ran과 tmp가 같을때까지 돌아가는 반복문 정의
    반복문에는 정수를 입력하라 출력하고 해당 값을 tmp에 담고
    tmp가 ran보다 크면 크다 알리고 작으면 작다 알리고
    n을 1 더한다.
    
    만약 같으면 정답과 n의 갯수를 출력한다

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level03/hard/Application2.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level03/hard/Application2.java
@@ -1,9 +1,11 @@
 package com.ohgiraffers.section02.looping_and_branching.level03.hard;
 
+import java.util.*;
+
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+
 		/* 1부터 100 사이의 난수를 발생시키고
 		 * 정수를 입력 받아서 입력받은 정수가 발생한 난수보다 큰 경우 "입력하신 정수보다 큽니다." 출력,
 		 * 입력받은 정수보다 난수가 작은 경우 "입력하신 정수보다 작습니다." 를 출력하며
@@ -23,7 +25,22 @@ public class Application2 {
 		 * 정수를 입력하세요 : 4
 		 * 정답입니다. 3회만에 정답을 맞추셨습니다.
 		 * */
-		
+		Scanner sc = new Scanner(System.in);
+		int ran;
+		int tmp;
+		int n = 0;
+
+		ran = (int)((Math.random() * 100) + 1);
+
+		do {
+			System.out.print("정수를 입력하세요 : ");
+			tmp = sc.nextInt();
+			if (tmp > ran) System.out.println("입력하신 정수보다 큽니다.");
+			else if (tmp < ran) System.out.println("입력하신 정수보다 작습니다.");
+			n++;
+		}while (ran != tmp);
+
+		System.out.println("정답입니다. " + n + "회만에 정답을 맞추셨습니다.");
 	}
 
 }
