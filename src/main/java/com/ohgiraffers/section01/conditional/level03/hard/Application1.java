170afd721eb2c8a953ffccc8eb71cf4f28097467
AzureAD\양한진 <gkswls3203@naver.com>
2023-05-04 13:52:00 +0900

    [conditional][hard][level 03-1] int는 정수를 담고 연산기호는 string 으로 담음 if, else if, else 활용하여 입력 연산기호에 따라 각각 다른 계산실행, 연산기호 미 입력시 프로그램 종료 메시지 출력

--- src/main/java/com/ohgiraffers/section01/conditional/level03/hard/Application1.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level03/hard/Application1.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section01.conditional.level03.hard;
 
+import java.util.Scanner;
+
 public class Application1 {
 	
 	public static void main(String[] args) {
@@ -16,7 +18,33 @@ public class Application1 {
 		 * -- 출력 예시 --
 		 * 4 + 3 = 7 
 		 */
-		
+		java.util.Scanner sc = new Scanner(System.in);
+		System.out.println("두개의 정수를 입력하고 연산기호(+, -, *, /, %)를 입력하시오");
+		int num1 = sc.nextInt();
+		System.out.println("첫번째 정수 :" + num1);
+		int num2 = sc.nextInt();
+		System.out.println("두번째 정수 :" + num2);
+		String a = sc.next();
+		System.out.println("연산기호를 입력하세요 : " + a);
+		if (a.equals("+")){
+			System.out.println(num1 + "+" + num2 + ""+ "=" + (num1 + num2));
+		}
+		else if (a.equals("-")) {
+			System.out.println(num1 + "-" + num2 + ""+ "=" + (num1-num2));
+		}
+		else if (a.equals("*")){
+			System.out.println(num1 + "*" + num2 + ""+ "=" + (num1*num2));
+		}
+		else if (a.equals("/")){
+			System.out.println(num1 + "/" + num2 + ""+ "=" + (num1/num2));
+		}
+		else if (a.equals("%")){
+			System.out.println(num1 + "%" + num2 + ""+ "=" + (num1%num2));
+		}
+		else {
+			System.out.println("입력하신 연산은 없습니다. 프로그램을 종료합니다.");
+		}
+
+		System.out.println("프로그램을 종료합니다.");
 	}
-	
 }
