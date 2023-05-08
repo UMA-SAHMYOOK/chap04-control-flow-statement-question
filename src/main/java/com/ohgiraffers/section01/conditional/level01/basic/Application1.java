087572c49037d1b6e36c0e7443be3e72cc050c32
GANG철\kang6 <kmc6381@gmail.com>
2023-05-09 01:42:46 +0900

    [basic][문제1] 완료

--- src/main/java/com/ohgiraffers/section01/conditional/level01/basic/Application1.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level01/basic/Application1.java
@@ -1,10 +1,21 @@
 package com.ohgiraffers.section01.conditional.level01.basic;
 
+import java.util.Scanner; //Scanner (API)들이 필요할 떄 넣어주기.
+
 public class Application1 {
 
 	public static void main(String[] args) {
-		
-		/* 정수를 하나 입력 받아 그 수가 양수이면 "양수다." 라고 출력하고,
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수를 하나 입력하세요: ");
+		int num = sc.nextInt();
+
+		if(num > 0) {
+			System.out.println("양수다");
+		} else System.out.println("양수가 아니다.");
+
+
+
+			/* 정수를 하나 입력 받아 그 수가 양수이면 "양수다." 라고 출력하고,
 		 * 양수가 아닌 경우 "양수가 아니다." 라고 출력하세요
 		 * 
 		 * -- 입력 예시 --
@@ -14,6 +25,6 @@ public class Application1 {
 		 * 양수다.
 		 * */
 
-	}
+		}
 
 }
